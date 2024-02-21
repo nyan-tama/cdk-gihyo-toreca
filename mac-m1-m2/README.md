f85ffa41d2a9d8c075bda27cbba185f9a47b4fe3
nyan-tama <shake.flake.love@gmail.com>
2024-02-21 11:01:05 +0900

    amdとarmの対応

---
+++ mac-m1-m2/README.md
@@ -0,0 +1,50 @@
+## Dockerの起動とDockerの中に入る
+```
+docker-compose run --rm cdk
+```
+コンテナから出る時は『exit』を入力して下さい。
+```
+exit
+```
+
+## CDK環境の初期設定
+```
+# 実行は初回時のみでOK CDKデプロイに必要な設定がAWSに上に作成される
+# すでに作成済みの場合は実行不要
+cdk bootstrap 
+```
+## AWS本番環境の構築 
+```
+cdk deploy
+```
+## EC2への接続 AWSのSSM接続を利用
+```
+aws ssm start-session --target EC2のインスタンスID
+sudo su - ec2-user
+```
+## githubからコードを読み込む
+```
+git clone https://github.com/あなたのgithubアカウント名/gihyo-toreca.git
+cd gihyo-toreca
+
+#実行したい各チャプターを指定
+cd chapter-1
+
+#READMEのマニュアルを参照
+cat README.md
+```
+
+## EC2の接続解除
+```
+exit
+```
+
+## AWS本番環境の削除
+```
+cdk destroy
+```
+
+## Dockerの終了
+```
+exit
+```
---
+++ mac-m1-m2/app.py
@@ -0,0 +1,62 @@
+from aws_cdk import (
+    App,
+    Stack,
+    aws_iam as iam,
+    aws_ec2 as ec2,
+    CfnOutput
+)
+from constructs import Construct
+
+class DiscordBotEC2Stack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        # VPCの作成
+        vpc = ec2.Vpc(self, "VPC", max_azs=2)
+
+        # IAMロールの作成
+        ec2_role = iam.Role(
+            self, "EC2Role",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+        )
+
+        # EC2セキュリティグループの定義
+        ec2_sg = ec2.SecurityGroup(
+            self, "DiscordBedrock-EC2-Sg",
+            vpc=vpc,
+            allow_all_outbound=True,
+        )
+
+        # EC2インスタンスの定義の前にユーザーデータスクリプトを定義
+        user_data = ec2.UserData.for_linux()
+        user_data.add_commands(
+            "sudo dnf update -y",  # システムの更新
+            "sudo dnf install -y git docker",  # GitとDockerのインストール
+            "sudo systemctl start docker",  # Dockerサービスの開始
+            "sudo systemctl enable docker",  # Dockerサービスの自動起動設定
+            "sudo usermod -aG docker ec2-user",  # ec2-userをdockerグループに追加
+        )
+
+        # EC2インスタンスの定義
+        ec2_instance = ec2.Instance(
+            self, "EC2Instance",
+            instance_type=ec2.InstanceType("t2.micro"),
+            machine_image=ec2.GenericLinuxImage({'ap-northeast-1': 'ami-012261b9035f8f938'}),
+            vpc=vpc,
+            role=ec2_role,
+            security_group=ec2_sg,
+            user_data=user_data, 
+            ssm_session_permissions=True  # SSMセッションマネージャのアクセスを許可
+        )
+
+        # SSMセッション開始コマンドの出力
+        CfnOutput(
+            self,
+            "StartSsmSessionCommand",
+            value=f"aws ssm start-session --target {ec2_instance.instance_id}",
+            description="上記のコマンドを実行してSSMを利用してEC2に接続してください",
+        )
+
+app = App()
+DiscordBotEC2Stack(app, "DiscordBotEC2Stack")
+app.synth()
